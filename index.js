1aa5feefafe051d8d9a83554b31f27b489326a5b
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-15 20:50:58 +0530

    Add logic to compute one digit operand at a time
    Woah! its working, but only takes single digit operand and 1 instance of expression

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ function operate(operand1, operand2, operator) {
 }
 
 const displayButton = document.querySelectorAll('.displayButton') // getting a node list
+const operand = document.querySelectorAll('.operand')
+const equalTo = document.querySelector('button:last-child')
 const display = document.querySelector('.display')
 
 // iterating over each value of the node list (forEach loop?)
@@ -39,10 +41,27 @@ const display = document.querySelector('.display')
 //         display.textContent = displayButton[i].textContent
 //     })
 // }
+
 let displayValue;
+let userInputOfNumber = []
+let userInputOfOperand = []
+
 displayButton.forEach((element) => {
-    element.addEventListener('click', ()=>{
+    element.addEventListener('click', () => {
         display.textContent = element.textContent;
         displayValue = display.textContent;
+        userInputOfNumber.push(displayValue)
+
     })
+})
+
+operand.forEach((element) => {
+    element.addEventListener('click', () => {
+        userInputOfOperand.push(element.textContent)
+    })
+})
+
+equalTo.addEventListener('click', () => {
+    let result = operate(+userInputOfNumber[0],+userInputOfNumber[1],userInputOfOperand[0]);
+    display.textContent = result;
 })
