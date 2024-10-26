2a5e4a6d3ce7d69d384b08b760c22e48d465af79
Vikas-Indora <indoravikas.99@gmail.com>
2024-10-26 15:06:53 +0530

    work as desired.. ig

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
-    return (a / b).toFixed(5);
+    if (a % b === 0) {
+        return a / b;
+    } else {
+        return (a / b).toFixed(2);
+    }
 }
 
 function operate(operand1, operand2, operator) {
@@ -29,3 +33,44 @@ const operator = document.querySelectorAll(".operator");
 const equalTo = document.querySelector("button:last-child");
 const allClear = document.querySelector("button:nth-child(14)");
 const display = document.querySelector(".display");
+
+let firstOperand;
+let secondOperand;
+let operation;
+let result;
+
+number.forEach(function (element) {
+    element.addEventListener("click", function () {
+        if (display.textContent == operation) {
+            display.textContent = null;
+        }
+        display.textContent += element.textContent;
+    });
+});
+
+operator.forEach(function (element) {
+    element.addEventListener("click", function () {
+        if (operation === undefined || operation === null) {
+            firstOperand = display.textContent;
+        }
+        operation = element.textContent;
+        if (display.textContent == result) {
+            firstOperand = display.textContent;
+        }
+        display.textContent = element.textContent;
+    });
+});
+
+equalTo.addEventListener("click", function () {
+    secondOperand = display.textContent;
+    result = operate(+firstOperand, +secondOperand, operation);
+    display.textContent = result;
+});
+
+allClear.addEventListener("click", function () {
+    display.textContent = null;
+    firstOperand = null;
+    secondOperand = null;
+    operation = null;
+    result = null;
+});
